f9434980751829abffa3d61da4be02763033b489
Niko <Nizh2008@gmail.com>
2015-08-04 09:59:20 +0800

    Initial Commit

---
+++ HitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitTest
+//
+//  Created by NikoTung on 8/4/15.
+//  Copyright © 2015 niko.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
